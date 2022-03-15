910ea8dd55290d8e09e3113b65e6c919740c79a1
ldz <lindikhaya.maqolwana@gmail.com>
2022-03-16 01:36:59 +0200

    ***

--- logic.js
+++ logic.js
@@ -2,11 +2,18 @@ let readMore = document.querySelector('.more');
 let moreButton = document.querySelector('.moreButton');
 const menuBar = document.querySelector('.menu-bar');
 const mobileMenu = document.querySelector('#mobile-menu');
+const closeMenu = document.querySelector('.bi-x');
 
 menuBar.addEventListener('click',()=>{
     mobileMenu.style.display = "block";
-})
+    menuBar.style.display = "none";
+});
+
+closeMenu.addEventListener('click',()=>{
+    menuBar.style.display = 'block';
+    mobileMenu.style.display ='none';
+});
+
 moreButton.addEventListener('click',()=>{
     readMore.style.display = 'inline';
-})
-console.log('we live')
+});
