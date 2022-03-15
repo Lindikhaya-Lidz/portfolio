174f0641ad9f387668493c46755ab050a6422748
ldz <lindikhaya.maqolwana@gmail.com>
2022-03-15 16:58:16 +0200

    added 3 illustrations

--- logic.js
+++ logic.js
@@ -1,6 +1,11 @@
 let readMore = document.querySelector('.more');
 let moreButton = document.querySelector('.moreButton');
+const menuBar = document.querySelector('.menu-bar');
+const mobileMenu = document.querySelector('#mobile-menu');
 
+menuBar.addEventListener('click',()=>{
+    mobileMenu.style.display = "block";
+})
 moreButton.addEventListener('click',()=>{
     readMore.style.display = 'inline';
 })
